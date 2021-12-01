5a2ad2380b32e871fa8e7a070373f5f4d06ce322
Michael Schubart <michael@schubart.net>
2021-12-01 15:13:15 +0000

    It's a list, not a list of lists
    The input contains no commas, so no need for split().

--- src/main/java/com/example/Day1.java
+++ src/main/java/com/example/Day1.java
@@ -3,13 +3,12 @@ package com.example;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Day1 {
 
     public static void main(String[] args) {
-        List<List<String>> depths = getDepths("depths.txt");
+        List<String> depths = getDepths("depths.txt");
         System.out.println("Final Part A count: " + countDepths(depths));
         System.out.println("Final Part B count: " + sumDepths(depths));
 
@@ -23,14 +22,13 @@ public class Day1 {
 
 
 
-    public static List<List<String>> getDepths(String input) {
+    public static List<String> getDepths(String input) {
         String file = ClassLoader.getSystemResource(input).getFile();
-        List<List<String>> depths = new ArrayList<>();
+        List<String> depths = new ArrayList<>();
             try (BufferedReader br = new BufferedReader(new FileReader(file))) {
                 String line;
                 while((line = br.readLine()) != null) {
-                    String[] values = line.split(",");
-                    depths.add(Arrays.asList(values));
+                    depths.add(line);
                 }
             }
             catch(Exception e) {
@@ -40,13 +38,13 @@ public class Day1 {
             return depths;
     }
 
-    public static Integer countDepths(List<List<String>> depths) {
+    public static Integer countDepths(List<String> depths) {
         Integer count = 0;
         for(int i = 1; i < depths.size(); i++) {
             Integer x = i;
             Integer y = i - 1;
-            Integer depthX = Integer.parseInt(depths.get(x).get(0));
-            Integer depthY = Integer.parseInt(depths.get(y).get(0));
+            Integer depthX = Integer.parseInt(depths.get(x));
+            Integer depthY = Integer.parseInt(depths.get(y));
             if(depthX > depthY) {
                 count++;
             }
@@ -55,13 +53,13 @@ public class Day1 {
         return count;
     }
 
-    public static Integer sumDepths(List<List<String>> depths) {
+    public static Integer sumDepths(List<String> depths) {
         Integer count = 0;
         for(int i = 3; i < depths.size(); i++) {
             Integer x = i;
             Integer y = i - 1;
-            Integer depthX = Integer.parseInt(depths.get(x).get(0)) + Integer.parseInt(depths.get(x-1).get(0)) + Integer.parseInt(depths.get(x-2).get(0));
-            Integer depthY = Integer.parseInt(depths.get(y).get(0)) + Integer.parseInt(depths.get(y-1).get(0)) + Integer.parseInt(depths.get(y-2).get(0));
+            Integer depthX = Integer.parseInt(depths.get(x)) + Integer.parseInt(depths.get(x-1)) + Integer.parseInt(depths.get(x-2));
+            Integer depthY = Integer.parseInt(depths.get(y)) + Integer.parseInt(depths.get(y-1)) + Integer.parseInt(depths.get(y-2));
             if(depthX > depthY) {
                 count++;
             }
