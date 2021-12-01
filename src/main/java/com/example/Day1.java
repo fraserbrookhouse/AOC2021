12cb067e84ebd7c0e0aac35feb65c653a1a5f554
Michael Schubart <michael@schubart.net>
2021-12-01 15:15:42 +0000

    Do String -> Integer conversion upfront

--- src/main/java/com/example/Day1.java
+++ src/main/java/com/example/Day1.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Day1 {
 
     public static void main(String[] args) {
-        List<String> depths = getDepths("depths.txt");
+        List<Integer> depths = getDepths("depths.txt");
         System.out.println("Final Part A count: " + countDepths(depths));
         System.out.println("Final Part B count: " + sumDepths(depths));
 
@@ -22,13 +22,13 @@ public class Day1 {
 
 
 
-    public static List<String> getDepths(String input) {
+    public static List<Integer> getDepths(String input) {
         String file = ClassLoader.getSystemResource(input).getFile();
-        List<String> depths = new ArrayList<>();
+        List<Integer> depths = new ArrayList<>();
             try (BufferedReader br = new BufferedReader(new FileReader(file))) {
                 String line;
                 while((line = br.readLine()) != null) {
-                    depths.add(line);
+                    depths.add(Integer.parseInt(line));
                 }
             }
             catch(Exception e) {
@@ -38,13 +38,13 @@ public class Day1 {
             return depths;
     }
 
-    public static Integer countDepths(List<String> depths) {
+    public static Integer countDepths(List<Integer> depths) {
         Integer count = 0;
         for(int i = 1; i < depths.size(); i++) {
             Integer x = i;
             Integer y = i - 1;
-            Integer depthX = Integer.parseInt(depths.get(x));
-            Integer depthY = Integer.parseInt(depths.get(y));
+            Integer depthX = depths.get(x);
+            Integer depthY = depths.get(y);
             if(depthX > depthY) {
                 count++;
             }
@@ -53,13 +53,13 @@ public class Day1 {
         return count;
     }
 
-    public static Integer sumDepths(List<String> depths) {
+    public static Integer sumDepths(List<Integer> depths) {
         Integer count = 0;
         for(int i = 3; i < depths.size(); i++) {
             Integer x = i;
             Integer y = i - 1;
-            Integer depthX = Integer.parseInt(depths.get(x)) + Integer.parseInt(depths.get(x-1)) + Integer.parseInt(depths.get(x-2));
-            Integer depthY = Integer.parseInt(depths.get(y)) + Integer.parseInt(depths.get(y-1)) + Integer.parseInt(depths.get(y-2));
+            Integer depthX = depths.get(x) + depths.get(x-1) + depths.get(x-2);
+            Integer depthY = depths.get(y) + depths.get(y-1) + depths.get(y-2);
             if(depthX > depthY) {
                 count++;
             }
